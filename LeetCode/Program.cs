49ac9c90209d937eccbc6b45942bf5bcd8b0ab45
Kyle Rebstock <krebstock@driverse.com>
2024-02-11 23:06:36 -0500

    moved the projects around

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,6 +6,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Begin");
             ProblemOne.TwoSum([1,2,3], 9);
             Console.ReadLine();
         }
